dc1329c8f4dc8e12ef48c922fbb6129989e7f0eb
Svetlin Sofroniev <svetlinbiserov@gmail.com>
2024-06-21 16:11:56 +0300

    Added get user info call.

--- src/app/services/user-service.service.ts
+++ src/app/services/user-service.service.ts
@@ -1,13 +1,37 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserServiceService {
   private http = inject(HttpClient);
+  private authService = inject(AuthService);
+  private username: any;
   constructor() { }
 
-  
+  useObject: any ={
+    "firstName": "",
+    "lastName": "",
+    "email":"",
+    "username":"",
+    "createdAt":"",
+    "updatedAt":""
+  }
+
+  getUserInfo(){
+   this.username = sessionStorage.getItem(this.authService.USER)
+
+    this.http.get('http://localhost:8082/onskill/users', this.username).subscribe(
+      {
+        next:res=>{
+          console.log(res);
+          
+        },
+        error: ()=> alert('Wrong credentials.')
+      }
+    );
+  }
 
 }
