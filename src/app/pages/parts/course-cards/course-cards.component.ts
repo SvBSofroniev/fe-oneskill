dc51240c34d691b97fa179968f0196bce87af578
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-06-24 02:17:17 +0300

    searching functionality

--- src/app/pages/parts/course-cards/course-cards.component.ts
+++ src/app/pages/parts/course-cards/course-cards.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, inject } from '@angular/core';
+import { AfterViewInit, Component, Input, OnChanges, SimpleChanges, inject } from '@angular/core';
 import { EnrolledVideo, VideoInfoResponseDTO, VideoService } from '../../../services/video.service';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
@@ -11,33 +11,47 @@ import { of } from 'rxjs';
   templateUrl: './course-cards.component.html',
   styleUrl: './course-cards.component.css'
 })
-export class CourseCardsComponent {
+export class CourseCardsComponent implements OnChanges {
+
   videoService = inject(VideoService);
   @Input() messageToPass: string = '';
 
   videos: VideoInfoResponseDTO[] = [];
   enrolled: EnrolledVideo[] = [];
-  
+  filteredVideos: VideoInfoResponseDTO[] = [];
+
   ngOnInit(): void {
     this.videoService.getVideosInfoData().subscribe(data => {
-      this.videos = data;    
+      this.videos = data;
+      this.filteredVideos = this.videos;
     });
     this.videoService.getEnrolledVideosData().subscribe(data => {
-      this.enrolled = data;    
-    });   
-    console.log(this.enrolled);
-         
+      this.enrolled = data;
+    });
   }
 
-  getImageSrc(thumbnailBase64: string): string {        
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['messageToPass'].currentValue) {
+      this.filterVideos(changes['messageToPass'].currentValue);
+    } else {
+      this.filteredVideos = this.videos;
+    }
+  }
+
+  filterVideos(value: string): void {
+    if (value !== '') {
+      this.filteredVideos = this.videos.filter(video =>
+        video.title.toLowerCase().includes(value.toLowerCase())
+      );
+    }
+  }
+
+  getImageSrc(thumbnailBase64: string): string {
     return `data:image/jpeg;base64,${thumbnailBase64}`;
   }
 
-   enroll(videoId: string): void {
-    console.log(videoId+ 'da');
-    
-    this.videoService.enrollToVideo(videoId).subscribe(data =>{
-      console.log(data + 'sdsd');
+  enroll(videoId: string): void {
+    this.videoService.enrollToVideo(videoId).subscribe(data => {
     });
   }
 
