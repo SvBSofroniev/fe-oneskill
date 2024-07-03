05cf45e1b9ad0a4aa1dc9c1a0d34345bbcca0d0e
Svetlin Sofroniev <svetlinbiserov@gmail.com>
2024-07-03 16:45:24 +0300

    Finished like and dislike mechanism

--- src/app/pages/video/video.component.ts
+++ src/app/pages/video/video.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, inject, NgZone, AfterViewInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, inject, NgZone, AfterViewInit, ViewChild, OnChanges, SimpleChanges } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { VideoInfoResponseDTO, VideoService } from '../../services/video.service';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
@@ -11,7 +11,7 @@ import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
   templateUrl: './video.component.html',
   styleUrl: './video.component.css'
 })
-export class VideoComponent implements OnInit, AfterViewInit {
+export class VideoComponent implements OnInit, AfterViewInit, OnChanges  {
   @ViewChild('videoPlayer')
   videoPlayer!: ElementRef<HTMLVideoElement>;
   videoService = inject(VideoService);
@@ -30,13 +30,27 @@ export class VideoComponent implements OnInit, AfterViewInit {
     likes: 0,
     dislikes: 0,
     views: 0
-
   };
+
+  likes = 0;
+  dislikes = 0;
+  dislikesTouched = false;
+  likesTouched = false;
 
 
   constructor(private ngZone: NgZone, private route: ActivatedRoute) {}
+  
+  
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+    
+  }
 
   ngOnInit(): void {
+    this.loadData();
+  }
+
+  loadData(){
     this.route.paramMap.subscribe((params) => {
       this.videoId = params.get('id');
     });
@@ -45,8 +59,21 @@ export class VideoComponent implements OnInit, AfterViewInit {
     });
     this.videoService.getVideoInfoData(this.videoId).subscribe({
       next: res => {
-        this.video = res}
-    })    
+        this.video = res
+        this.likes = this.video.likes;
+        this.dislikes = this.video.dislikes;
+      }
+    })
+  }
+
+  reloadVideoInfoData(){
+    this.videoService.getVideoInfoData(this.videoId).subscribe({
+      next: res => {
+        this.video = res
+        this.likes = this.video.likes;
+        this.dislikes = this.video.dislikes;
+      }
+    })
   }
   ngAfterViewInit() {
     const video = this.videoPlayer.nativeElement;
@@ -72,9 +99,56 @@ export class VideoComponent implements OnInit, AfterViewInit {
     this.interact(id, 'dislike');
   }
 
-  interact(id: string, action:string){
-    this.videoService.interact(id, action).subscribe();
+  interact(id: string, action: string): void {
+    // Optimistic UI update
+    if (action === 'like') {
+      this.likes++;
+      this.likesTouched = true;
+      console.log(1);
+      
+      if (this.dislikesTouched) {
+        console.log(2);
+
+        this.dislikes--;
+        this.dislikesTouched = false;
+      }
+    } else if (action === 'dislike') {
+      console.log(3);
+
+      this.dislikes++;
+      this.dislikesTouched = true;
+      if (this.likesTouched) {
+        console.log(4);
+
+        this.likes--;
+        this.likesTouched = false;
+      }
+    }
+
+    this.videoService.interact(id, action).subscribe({
+      next: () => {
+        // Reload the video info to get the latest likes/dislikes count from the server
+        console.log(5);
+
+        this.reloadVideoInfoData();
+      },
+      error: () => {
+        // Rollback the optimistic update in case of an error
+        
+        console.log(6);
+        
+        if (action === 'like') {
+          this.likes--;
+          this.likesTouched = false;
+        } else if (action === 'dislike') {
+          this.dislikes--;
+          this.dislikesTouched = false;
+        }
+      }
+    });
   }
+
+
 }
 
 
